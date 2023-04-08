ad771b040dae64ed245a00277457a3441df61b19
pepep <pepep@Matte>
2023-04-08 01:08:52 -0400

    initial commit

---
+++ src/demo/Day2HW.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Day2HW {
+
+	public static void main (String [] args) {
+		System.out.println("Day2HWprint");
+}
+	public void getInfo() {
+	System.out.println("Push..");
+}
+}
